cfcaef3663e3ae0c5018ef65065eee8e6afd19b5
Kylian94 <kylian.petitgenet@hotmail.fr>
2019-06-24 17:44:25 +0200

    add Vue system with Laravel OAuth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,70 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue'
+    import VueRouter from 'vue-router'
 
-require('./bootstrap');
+    Vue.use(VueRouter)
 
-window.Vue = require('vue');
+    import App from './components/App'
+    import Home from './components/Home'
+    import Login from './components/Login'
+    import Register from './components/Register'
+    
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+    const router = new VueRouter({
+        mode: 'history',
+        routes: [
+            
+            {
+                path: '/',
+                name: 'home',
+                component: Home
+            },
+            {
+                path: '/login',
+                name: 'login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'register',
+                component: Register
+            },
+        ],
+    })
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+    router.beforeEach((to, from, next) => {
+        if (to.matched.some(record => record.meta.requiresAuth)) {
+            if (localStorage.getItem('bigStore.jwt') == null) {
+                next({
+                    path: '/login',
+                    params: { nextUrl: to.fullPath }
+                })
+            } else {
+                let user = JSON.parse(localStorage.getItem('bigStore.user'))
+                if (to.matched.some(record => record.meta.is_admin)) {
+                    if (user.is_admin == 1) {
+                        next()
+                    }
+                    else {
+                        next({ name: 'userboard' })
+                    }
+                }
+                else if (to.matched.some(record => record.meta.is_user)) {
+                    if (user.is_admin == 0) {
+                        next()
+                    }
+                    else {
+                        next({ name: 'admin' })
+                    }
+                }
+                next()
+            }
+        } else {
+            next()
+        }
+    })
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+    const app = new Vue({
+        el: '#app',
+        components: { App },
+        router,
+    });
