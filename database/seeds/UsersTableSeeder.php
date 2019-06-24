89058f5618a662f9ac79ac9ae2803d30ad01e80d
Karine <boubertkarine@gmail.com>
2019-06-24 18:04:00 +0200

    rr

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,11 +4,7 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
         for($i=0;$i<20;$i++) {
